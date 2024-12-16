dd93975a2e4de3a2e2a155bfc386f5af2224354a
Carlos Lima <inv.clima@ifood3rd.com>
2024-12-16 18:04:14 -0300

    criado filterchips e list de filterchips pra cada categoria selecionada

---
+++ app/src/main/java/com/chslcompany/nearbyrocketseat/ui/components/button/NearbyButton.kt
@@ -0,0 +1,78 @@
+package com.chslcompany.nearbyrocketseat.ui.components.button
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chslcompany.nearbyrocketseat.R
+import com.chslcompany.nearbyrocketseat.ui.theme.GreenBase
+import com.chslcompany.nearbyrocketseat.ui.theme.MyTypography
+
+@Composable
+fun NearbyButton(
+    modifier: Modifier = Modifier,
+    text: String? = null,
+    @DrawableRes iconRes: Int? = null,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier.heightIn(min = 56.dp),
+        shape = RoundedCornerShape(16.dp),
+        contentPadding = if (text == null && iconRes != null) {
+            PaddingValues(0.dp)
+        } else ButtonDefaults.ContentPadding,
+        colors = ButtonDefaults.buttonColors(containerColor = GreenBase),
+        onClick = onClick
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            iconRes?.let {
+                Icon(painter = painterResource(id = iconRes), contentDescription = "Icone do botão")
+            }
+            text?.let { Text(text = text.uppercase(), style = MyTypography.labelLarge) }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun NearbyButtonPreview() {
+    NearbyButton(
+        modifier = Modifier.fillMaxWidth(),
+        text = "Confirmar",
+        iconRes = R.drawable.ic_scan
+    ) {}
+}
+
+@Preview
+@Composable
+private fun NearbyButtonNoTextPreview() {
+    NearbyButton(
+        modifier = Modifier,
+        iconRes = R.drawable.ic_arrow_left
+    ) {}
+}
+
+@Preview
+@Composable
+private fun NearbyButtonNoIconPreview() {
+    NearbyButton(
+        modifier = Modifier.fillMaxWidth(),
+        text = "Confirmar"
+    ) {}
+}
