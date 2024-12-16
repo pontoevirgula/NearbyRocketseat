6d52159674d2e6e667bda6a725f400590b065d19
Carlos Lima <inv.clima@ifood3rd.com>
2024-12-16 18:01:59 -0300

    add arquivos ao gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.chslcompany.nearbyrocketseat"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.chslcompany.nearbyrocketseat"
